09661faed8d5d663f00795ad0f1e534f52945bf7
basonpark <bp2470@columbia.edu>
2025-04-24 12:19:42 +0900

    front end ui, core logic for backend: slither, mythril, openai for analysis, fastapi and uvicorn for creating api

--- server/dist/routes/audit.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const audit_controller_1 = require("../controllers/audit.controller");
-const router = (0, express_1.Router)();
-// Route to handle contract submission
-// POST /api/audit
-router.post('/', audit_controller_1.submitContractForAudit);
-exports.default = router;
